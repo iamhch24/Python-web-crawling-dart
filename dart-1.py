19c5a9e2f61a3142885cd7e450f47565bf5937d3
iamhch24 <iamhch24@gmail.com>
2020-11-18 11:14:33 +0900

    현충호가 dart-1.py 코드를 추가함. 에러 메시지를 출력할 수 있다. 회사이름을 뽑아내는 코드 작성.

--- dart-1.py
+++ dart-1.py
@@ -5,7 +5,8 @@ AUTH_KEY = 'a1085e73b6f15fed673b1a8c363789120bef84f0'
 args = {
     'bgn_de':'20201001',
     'end_de':'20201101',
-    'copr_cls':'K'
+    'copr_cls':'K',
+    'sort':'date'
 }
 
 args_str = ''
@@ -16,7 +17,14 @@ url = 'https://opendart.fss.or.kr/api/list.json?crtfc_key=%s%s' %(AUTH_KEY, args
 print(url+"\n")
 res = requests.get(url)
 data = res.json()
-print(data)
+# print(data)
 
+if data['status'] != '000':
+    print(datap['message'])
+else:
+    data_list = data['list']
+    for d in data_list:
+        print(d['corp_name'])
+        
 
 
