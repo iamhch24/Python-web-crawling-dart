52870253191af4b966ad75493948267ac64e2620
iamhch24 <iamhch24@gmail.com>
2020-11-18 10:43:53 +0900

    dart-1.py first upload

---
+++ dart-1.py
@@ -0,0 +1,22 @@
+import requests
+
+AUTH_KEY = 'a1085e73b6f15fed673b1a8c363789120bef84f0'
+
+args = {
+    'bgn_de':'20201001',
+    'end_de':'20201101',
+    'copr_cls':'K'
+}
+
+args_str = ''
+for k, v in args.items():
+    args_str += '&%s=%s' %(k,v)
+
+url = 'https://opendart.fss.or.kr/api/list.json?crtfc_key=%s%s' %(AUTH_KEY, args_str)
+print(url+"\n")
+res = requests.get(url)
+data = res.json()
+print(data)
+
+
+
